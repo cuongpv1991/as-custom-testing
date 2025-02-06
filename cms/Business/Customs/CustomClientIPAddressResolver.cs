84bdd6bf2f8c9e4e16b95b1c34b82b2d396cd0e0
Cuong Phan <cuongpv1991@gmail.com>
2025-02-06 07:40:03 +0700

    Create CustomClientIPAddressResolver.cs

---
+++ cms/Business/Customs/CustomClientIPAddressResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using EPiServer.Personalization;
+
+namespace cms.Business.Customs
+{
+    /// <summary>
+    /// refer: https://www.patrickvankleef.com/2015/06/07/using-dojo-when-creating-a-custom-visitor-group/
+    /// </summary>
+    public class CustomClientIPAddressResolver : IClientIPAddressResolver
+    {
+        public IPAddress ResolveAddress(HttpContextBase httpContext)
+        {
+            IPAddress pAddress;
+            if (httpContext.Request.UserHostAddress.Equals("::1"))  // Website is running localhost so define hardcoded a valid IP address
+            {
+                return IPAddress.Parse("0.0.0.0");
+            }
+            else if (IPAddress.TryParse(httpContext.Request.UserHostAddress, out pAddress))
+            {
+                return pAddress;
+            }
+            return IPAddress.None;
+        }
+    }
+}
